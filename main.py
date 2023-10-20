f059a8f06b519eceda7336cae96435a2899e8cd9
_90_ <y97315233+git@gmail.com>
2023-10-19 22:14:58 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pytube import YouTube 
+import os
+import Searcher
+import VideoTrimmer
+import EndVideo
+
+# Searcher.Clear()
+# VideoTrimmer.Clear()
+
+# Searcher.Src("the smiths", 10) 
+VideoTrimmer.Trim()
+
+EndVideo.Combine()
