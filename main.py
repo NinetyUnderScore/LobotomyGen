03177fd55043ef1849f633de25aaf5a806dfe1ff
_90_ <81204025+NinetyUnderScore@users.noreply.github.com>
2023-10-20 11:46:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from pytube import YouTube 
 import os
-import Searcher
-import VideoTrimmer
-import ChannelIndexer
-import EndVideo
+import Searcher, VideoTrimmer, ChannelIndexer, EndVideo
+
+if not os.path.exists(os.path.join(os.getcwd(), "videos")):
+    os.mkdir(os.path.join(os.getcwd(),"videos"))
+if not os.path.exists(os.path.join(os.getcwd(), "videosTrimmed")):
+    os.mkdir(os.path.join(os.getcwd(),"videosTrimmed"))
 
 # Searcher.Clear()
-Searcher.Src(input("Input prompt:"), 5) 
+# Searcher.Src(input("Input prompt:"), 5) 
 
 # ChannelIndexer.Clear()
 # ChannelIndexer.Index(input("Input playlist:"), 10)
 
 # VideoTrimmer.Clear()
-VideoTrimmer.Trim() 
+# VideoTrimmer.Trim() 
 
-EndVideo.Combine()
+# EndVideo.Combine()
